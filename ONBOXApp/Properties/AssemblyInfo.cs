1144b11d5e7e3624be41fc2c3f03c373e0393320
Thiago Augusto Barros de Almeida <thy-almeida@hotmail.com>
2019-06-19 18:48:23 +0930

    Changed version to 1.0.0 in preparation for adsk store. Also automated builds to place files in .bundle folder

--- ONBOXApp/Properties/AssemblyInfo.cs
+++ ONBOXApp/Properties/AssemblyInfo.cs
@@ -7,11 +7,11 @@ using System.Windows;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("ONBOXAppl")]
-[assembly: AssemblyDescription("Aplicativo de Revit da ONBOX")]
+[assembly: AssemblyTitle("OnboxApp")]
+[assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("ONBOX design!")]
-[assembly: AssemblyProduct("ONBOXAppl")]
+[assembly: AssemblyCompany("Onbox")]
+[assembly: AssemblyProduct("OnboxApp")]
 [assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -51,5 +51,5 @@ using System.Windows;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.8.0.0")]
-[assembly: AssemblyFileVersion("0.8.0.0")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
